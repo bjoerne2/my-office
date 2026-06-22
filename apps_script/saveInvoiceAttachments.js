3b9e77e2baa039a322e261c0b9cf1e871f2af261
Björn Weinbrenner <info@bjoerne.com>
2026-06-22 22:56:23 +0200

    consider new GMail labels

--- apps_script/saveInvoiceAttachments.js
+++ apps_script/saveInvoiceAttachments.js
@@ -1,43 +1,77 @@
 function saveInvoiceAttachments() {
 
   const folderId = "1EFJU_q7SugjvFlQkdcQ-Wrs5s48HLHLQ";
-  const folder = DriveApp.getFolderById(folderId);
+  const baseFolder = DriveApp.getFolderById(folderId);
   const doneLabel = GmailApp.getUserLabelByName("Done") || GmailApp.createLabel("Done");
-  const threads = GmailApp.search('label:Buchführung -label:Done');
-  threads.forEach(thread => {
-    let savedCount = 0;
-    const messages = thread.getMessages();
-    messages.forEach(message => {
-      Logger.log(
-        "E-Mail gefunden: Betreff='" + message.getSubject() +
-        "', Von='" + message.getFrom() +
-        "', Datum='" + message.getDate() + "'"
-      );
-      const attachments = message.getAttachments();
-      attachments.forEach(att => {
-        const contentType = att.getContentType();
-        const fileName = att.getName();
-        const isPdfAttachment = contentType === "application/pdf" ||
-          (contentType === "application/octet-stream" && fileName.toLowerCase().endsWith(".pdf"));
+  const labelConfigs = [
+    { gmailLabel: "Buchführung/AWS", driveFolderName: "AWS" },
+    { gmailLabel: "Buchführung/GitHub", driveFolderName: "GitHub" }
+  ];
 
+  labelConfigs.forEach(config => {
+    const targetFolder = getOrCreateSubFolder(baseFolder, config.driveFolderName);
+    const threads = GmailApp.search('label:"' + config.gmailLabel + '" -label:Done');
+
+    Logger.log(
+      "Verarbeite Label '" + config.gmailLabel + "' in Drive-Unterordner '" +
+      config.driveFolderName + "'. Gefundene Threads: " + threads.length
+    );
+
+    threads.forEach(thread => {
+      let savedCount = 0;
+      const messages = thread.getMessages();
+      messages.forEach(message => {
         Logger.log(
-          "Attachment gefunden: Name='" + fileName +
-          "', Content-Type='" + contentType + "'"
+          "E-Mail gefunden: Label='" + config.gmailLabel +
+          "', Betreff='" + message.getSubject() +
+          "', Von='" + message.getFrom() +
+          "', Datum='" + message.getDate() + "'"
         );
-        if (isPdfAttachment) {
-          const existingFiles = folder.getFilesByName(fileName);
-          if (existingFiles.hasNext()) {
-            Logger.log("Übersprungen, Datei existiert bereits in Drive: " + fileName);
-          } else {
-            folder.createFile(att);
-            savedCount += 1;
-            Logger.log("Gespeichert: " + fileName);
-          }
-        }
-      });
-    });
+        const attachments = message.getAttachments();
+        attachments.forEach(att => {
+          const contentType = att.getContentType();
+          const fileName = att.getName();
+          const isPdfAttachment = contentType === "application/pdf" ||
+            (contentType === "application/octet-stream" && fileName.toLowerCase().endsWith(".pdf"));
 
-    thread.addLabel(doneLabel);
-    Logger.log("Thread als Done markiert. Gespeicherte PDFs: " + savedCount);
+          Logger.log(
+            "Attachment gefunden: Label='" + config.gmailLabel +
+            "', Name='" + fileName +
+            "', Content-Type='" + contentType + "'"
+          );
+          if (isPdfAttachment) {
+            const existingFiles = targetFolder.getFilesByName(fileName);
+            if (existingFiles.hasNext()) {
+              Logger.log(
+                "Übersprungen, Datei existiert bereits in Drive-Unterordner '" +
+                config.driveFolderName + "': " + fileName
+              );
+            } else {
+              targetFolder.createFile(att);
+              savedCount += 1;
+              Logger.log(
+                "Gespeichert in '" + config.driveFolderName + "': " + fileName
+              );
+            }
+          }
+        });
+      });
+
+      thread.addLabel(doneLabel);
+      Logger.log(
+        "Thread für Label '" + config.gmailLabel +
+        "' als Done markiert. Gespeicherte PDFs: " + savedCount
+      );
+    });
   });
+}
+
+function getOrCreateSubFolder(parentFolder, folderName) {
+  const folders = parentFolder.getFoldersByName(folderName);
+  if (folders.hasNext()) {
+    return folders.next();
+  }
+
+  Logger.log("Drive-Unterordner wird erstellt: " + folderName);
+  return parentFolder.createFolder(folderName);
 }
