5969b5d6f296c09226d5b0bac0d0abed6e6787b5
Björn Weinbrenner <info@bjoerne.com>
2026-06-22 22:01:48 +0200

    consider Done label

--- apps_script/saveInvoiceAttachments.js
+++ apps_script/saveInvoiceAttachments.js
@@ -2,17 +2,23 @@ function saveInvoiceAttachments() {
 
   const folderId = "1EFJU_q7SugjvFlQkdcQ-Wrs5s48HLHLQ";
   const folder = DriveApp.getFolderById(folderId);
-  const threads = GmailApp.search('label:Buchführung');
+  const doneLabel = GmailApp.getUserLabelByName("Done") || GmailApp.createLabel("Done");
+  const threads = GmailApp.search('label:Buchführung -label:Done');
   threads.forEach(thread => {
+    let savedCount = 0;
     const messages = thread.getMessages();
     messages.forEach(message => {
       const attachments = message.getAttachments();
       attachments.forEach(att => {
         if (att.getContentType() === "application/pdf") {
           folder.createFile(att);
+          savedCount += 1;
           Logger.log("Gespeichert: " + att.getName());
         }
       });
     });
+
+    thread.addLabel(doneLabel);
+    Logger.log("Thread als Done markiert. Gespeicherte PDFs: " + savedCount);
   });
 }
